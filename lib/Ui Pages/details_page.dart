7a802a39725d99333e4a148f10dcf0525f6d32c7
msnsadq <nannatsadik@gmail.com>
2022-05-16 15:26:44 +0530

    added more futures

--- lib/Ui Pages/details_page.dart
+++ lib/Ui Pages/details_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:onlineshopping/BLOC%20product%20id/bloc_product_id_bloc.dart';
 import 'package:onlineshopping/Model%20Classes/ProductIdModel.dart';
@@ -24,6 +26,18 @@ class _DetailsPageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+bottomNavigationBar: BottomNavigationBar(
+    backgroundColor: Colors.black,
+    selectedItemColor: Colors.white,
+    unselectedItemColor: Colors.white,
+    selectedIconTheme: IconThemeData(color: Colors.white),
+    unselectedIconTheme : IconThemeData(color: Colors.white),
+   // selectedLabelStyle: TextStyle(backgroundColor: Colors.white),
+   // unselectedLabelStyle: TextStyle(backgroundColor: Colors.orange),
+    items: [
+  BottomNavigationBarItem(icon: Icon(Icons.card_travel),label: "GO TO CART"),
+  BottomNavigationBarItem(icon: Icon(Icons.add_shopping_cart),label: "BUY NOW"),
+]),
       appBar: AppBar(backgroundColor: Colors.black,),
       backgroundColor: Colors.black,
       body: BlocBuilder<ProductIdBloc,ProductIdState>(builder: (context,state){
@@ -34,104 +48,101 @@ class _DetailsPageState extends State<DetailsPage> {
         }
         if(state is ProductIdLoaded){
           productIdModel = BlocProvider.of<ProductIdBloc>(context).productIdModel;
-          return  SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Padding(
-              padding: EdgeInsets.only(left: 5,right: 5),
-              child: Column(
-               // crossAxisAlignment: CrossAxisAlignment.center,
+          return  Padding(
+            padding: EdgeInsets.only(left: 5,right: 5),
+            child: Column(
+             // crossAxisAlignment: CrossAxisAlignment.center,
 //mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 200,
-                      child: Card(
-                        child: Image.network(widget.images,fit: BoxFit.fill,),
-                      ),
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height*.5
+                    ,
+                    child: Card(
+                      child: Image.network(widget.images,fit: BoxFit.fill,),
                     ),
                   ),
-                  Row(
-                    children: [
-                      SizedBox(width: 5,),
-                      Text(productIdModel[0].productName.toString(),style: TextStyle(fontSize: 20,color: Colors.white),),
-                      Text("( ${productIdModel[0].description.toString()} )",style: GoogleFonts.aleo(color: Colors.white),)
-                    ],
-                  ),
-                 // Text(productIdModel[0].description.toString(),style: GoogleFonts.allerta (color: Colors.white),),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: [
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: 5,),
+                    Text(productIdModel[0].productName.toString(),style: TextStyle(fontSize: 20,color: Colors.white),),
+                    Text("( ${productIdModel[0].description.toString()} )",style: GoogleFonts.aleo(color: Colors.white),)
+                  ],
+                ),
+               // Text(productIdModel[0].description.toString(),style: GoogleFonts.allerta (color: Colors.white),),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
 SizedBox(width: 10,),
-                      Icon(Icons.money_off_sharp,color: Colors.white,),
-                      Text(productIdModel[0].price.toString(),style: TextStyle(color: Colors.white),),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only( top:8.0,left: 3,right: 3),
-                        child: SizedBox(
+                    Icon(Icons.money_off_sharp,color: Colors.white,),
+                    Text(productIdModel[0].price.toString(),style: TextStyle(color: Colors.white),),
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only( top:8.0,left: 3,right: 3),
+                      child: SizedBox(
 
-                          child: Card(
-                            color: Colors.black,
-                            elevation: 20,
-                            child: Text("160 people ordered this in the last 30 days",style: GoogleFonts.alike(color: Colors.white),),
-                          ),
+                        child: Card(
+                          color: Colors.black,
+                          elevation: 20,
+                          child: Text("160 people ordered this in the last 30 days",style: GoogleFonts.alike(color: Colors.white),),
                         ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 5,),
-                  Row(
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 15,),
+                Container(
+                  color: Colors.black,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Column(
                         children: [
-
+                   Icon(Icons.keyboard_return,color: Colors.white,),
+                          Text("7 Days Returns",style: TextStyle(color: Colors.white),)
+                        ],
+                      ),
+                      //VerticalDivider(width: 5,color: Colors.black,thickness: 3,),
+                      Column(
+                        children: [
+                          Icon(Icons.money,color: Colors.white,),
+                          Text("Cash On Delivery available",style: TextStyle(color: Colors.white),)
                         ],
                       ),
                       Column(
-                        children: [],
-                      ),
-                      Column(
-                        children: [],
+                        children: [
+                          Icon(Icons.add,color: Colors.white,),
+                          Text("Plus Product",style: TextStyle(color: Colors.white),)
+                        ],
                       )
                     ],
-                  )
-                ],
+                  ),
+                ),
 
-              ),
+              ],
+
             ),
-          ) ; 
-              
-              
-          //   Column(
-          //   // mainAxisAlignment: MainAxisAlignment.center,
-          //    //crossAxisAlignment: CrossAxisAlignment.center,
-          //   children: [
-          //     Align(
-          //       alignment: Alignment.center,
-          //       child: Card(
-          //         elevation: 20,
-          //         child:
-          //             Text(productIdModel[0].productName.toString(),style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),)
-          //
-          //       ),
-          //     )
-          //   ],
-          // );
-          
+          ) ;
+
+
+
+
         }
         if(state is ProductIdError){
           return Center(
